aa52987fbbaea66ca4e22fd99cf6356d6587843d
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-17 22:10:26 -0400

    Actualizar levelup.js

--- plugins/levelup.js
+++ plugins/levelup.js
@@ -1,58 +1,63 @@
-import fetch from 'node-fetch';
-import { canLevelUp, xpRange } from '../lib/levelling.js';
+import { canLevelUp, xpRange } from '../lib/levelling.js'
+import fetch from 'node-fetch'
+import fs from 'fs'
+import { RankCardBuilder } from 'discord-card-canvas'
 
-let handler = async (m, { conn, usedPrefix }) => {
-  let who = m.sender;
-  let name = conn.getName(m.sender);
-  let discriminator = who.substring(9, 13);
-  let pp = 'https://i.pinimg.com/564x/d4/17/75/d41775c2a051fe94269e71bdcbd240a5.jpg';
-  
-  try {
-    pp = await conn.profilePictureUrl(m.sender, 'image');
-  } catch (e) {
-  } finally {
-    let user = global.db.data.users[m.sender];
-    let { min, xp, max } = xpRange(user.level, global.multiplier);
+let handler = async (m, { conn }) => {
+    let name = conn.getName(m.sender)
+    let user = global.db.data.users[m.sender]
+    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 
     if (!canLevelUp(user.level, user.exp, global.multiplier)) {
-      await conn.reply(m.chat, `Level ${name} ${user.level} (${user.exp - min}/${xp})\nKurang ${max - user.exp} EXP lagi!`.trim(), m);
+        let { min, xp, max } = xpRange(user.level, global.multiplier)
+        let txt = ` –  *L E V E L U P  -  U S E R*\n\n`
+            txt += `✩ *Nombre* : ${name}\n`
+            txt += `✩ *Nivel* : ${user.level}\n`
+            txt += `✩ *XP* : ${user.exp - min}/${xp}\n\n`
+            txt += `Te falta *${max - user.exp}* de *💫 XP* para subir de nivel`
+
+        const canvasRank = await new RankCardBuilder({
+            currentLvl: user.level,
+            currentRank: 0,
+            currentXP: user.exp - min,
+            requiredXP: xp,
+            backgroundColor: { background: '#070d19', bubbles: '#0ca7ff' },
+            avatarImgURL: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
+            nicknameText: { content: name, font: 'Nunito', color: '#0CA7FF' },
+            userStatus: 'online',
+        }).build()
+
+        await conn.sendFile(m.chat, canvasRank.toBuffer(), 'rank.png', txt, m, null, fake)
+        return
     }
 
-    let before = user.level * 1;
-
-    while (canLevelUp(user.level, user.exp, global.multiplier)) {
-      user.level++;
-    }
-
+    let before = user.level * 1
+    while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
     if (before !== user.level) {
-      await conn.reply(m.chat, `${name} Level Up!\n_${before}_ -> ${user.level}`.trim(), m);
+        let txt = ` –  *L E V E L U P  -  U S E R*\n\n`
+            txt += `✩ *Nombre* : ${name}\n`
+            txt += `✩ *Nivel Anterior* : ${before}\n`
+            txt += `✩ *Nivel Actual* : ${user.level}\n\n`
+            txt += `🤍 Cuanto más interactúes con *Ai Genesis*, mayor será tu Nivel`
+
+
+        const canvasRank = await new RankCardBuilder({
+            currentLvl: user.level,
+            currentRank: 0,
+            currentXP: user.exp - min,
+            requiredXP: xp,
+            backgroundColor: { background: '#070d19', bubbles: '#0ca7ff' },
+            avatarImgURL: 'https://telegra.ph/file/b97148e2154508f63d909.jpg',
+            nicknameText: { content: name, font: 'Nunito', color: '#0CA7FF' },
+            userStatus: 'online',
+        }).build()
+
+        await conn.sendFile(m.chat, canvasRank.toBuffer(), 'rank.png', txt, m, null, fake)
     }
-    // Mengirim gambar profil
-    await conn.sendFile(m.chat, pp, 'profile.jpg', 'Ini foto profil kamu!', m);
-
-    // Jeda agar pesan terlihat berurutan
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    
-  }
 }
 
-handler.help = ['levelup'];
-handler.tags = ['xp'];
-handler.command = /^levelup$/i;
-
-export default handler;
-function sort(property, ascending = true) {
-  if (property) return (...args) => args[ascending & 1][property] - args[!ascending & 1][property]
-  else return (...args) => args[ascending & 1] - args[!ascending & 1]
-}
-
-function toNumber(property, _default = 0) {
-  if (property) return (a, i, b) => {
-    return { ...b[i], [property]: a[property] === undefined ? _default : a[property] }
-  }
-  else return a => a === undefined ? _default : a
-}
-
-function enumGetKey(a) {
-  return a.jid
-}
+handler.help = ['levelup']
+handler.tags = ['rpg']
+handler.command = ['nivel', 'lvl', 'levelup', 'level'] 
+handler.register = true 
+export default handler
