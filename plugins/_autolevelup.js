295521dfe005dd7841ce534c3272c737e989d410
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-18 20:20:25 -0400

    Actualizar _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -1,39 +1,4 @@
-import { canLevelUp, xpRange } from '../lib/levelling.js'
-import { levelup } from '../lib/canvas.js'
-import moment from 'moment-timezone'
-export async function before(m, { conn }) {
-    let user = global.db.data.users[m.sender]
-    let chat = global.db.data.chats[m.chat]
-    if (m.isGroup && chat.autolevelup && !chat.isBanned) {
-        if (canLevelUp(user.level, user.exp, global.multiplier)) {
-            let before = user.level * 1
-            while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
-            if (before !== user.level) {
-                let teks = `.             ${user.role}`
-                let str = `
-*🎉 C O N G R A T S 🎉*
-*${before}* ➔ *${user.level}* [ *${user.role}* ]
-
-*Note:* _Semakin sering berinteraksi dengan bot Semakin Tinggi level kamu_
-`.trim()
-                try {
-                    let img = await levelup(teks, user.level)
-                    await conn.sendFile(m.chat, img, 'global.thumblvlup', str, m)
-                } catch (e) {
-                    let img = await levelup(teks, user.level)
-                    await conn.sendFile(m.chat, img, 'global.thumblvlup', str, m)
-                }
-            }
-        }
-        return !0
-    }
-    return !0
-}
-
-
-
-
-/* import { xpRange, canLevelUp, findLevel } from '../lib/levelling.js'
+import { xpRange, canLevelUp, findLevel } from '../lib/levelling.js'
 import fetch from 'node-fetch'
 
 let handler = m => m
@@ -87,4 +52,4 @@ function toNumber(property, _default = 0) {
 
 function enumGetKey(a) {
         return a.jid
-} */
+}
