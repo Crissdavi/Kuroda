d7214de718522d17302f88f3e41c6a5b578c1057
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-12-20 18:01:33 -0500

    Actualizar _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -99,7 +99,7 @@ global.fake = {
     contextInfo: {
             isForwarded: true,
     forwardedNewsletterMessageInfo: {
-      newsletterJid: "120363220939514640@newsletter",
+      newsletterJid: "120363290084838811@newsletter",
       serverMessageId: 100,
       newsletterName: '❀ 𝑵 𝑬 𝒁 𝑼 ❀',
     }, 
