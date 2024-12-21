fc9582f45ff288683c6b663e4491f610a647730f
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-12-21 09:05:49 -0500

    Actualizar anime-ppcp.js

--- plugins/anime-ppcp.js
+++ plugins/anime-ppcp.js
@@ -5,7 +5,7 @@ const res = await Couples()
 await conn.sendFile(m.chat, res.male, 'ppcp.jpg', 'Male', m) 
 return conn.sendFile(m.chat, res.female, 'ppcp.jpg', 'Female', m)
 }
-handler.help = ['ppcouple']
+handler.help = ['Sharephotos']
 handler.tags = ['internet']
-handler.command = /^(ppcp|ppcouple)$/i
+handler.command = /^(Sharephotos|compartirimagen|compartirfoto)$/i
 export default handler
