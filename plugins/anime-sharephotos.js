7c89db786fbffb8abdcd46683e2e203f92bd2740
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-12-21 11:36:41 -0500

    Actualizar anime-sharephotos.js

--- plugins/anime-sharephotos.js
+++ plugins/anime-sharephotos.js
@@ -2,8 +2,8 @@ import { Couples } from 'dhn-api'
 var handler = async (m, { conn, command }) => {
 
 const res = await Couples() 
-await conn.sendFile(m.chat, res.male, 'ppcp.jpg', 'Male', m) 
-return conn.sendFile(m.chat, res.female, 'ppcp.jpg', 'Female', m)
+await conn.sendFile(m.chat, res.male, 'ppcp.jpg', 'Hombre', m) 
+return conn.sendFile(m.chat, res.female, 'ppcp.jpg', 'Mujer', m)
 }
 handler.help = ['Sharephotos']
 handler.tags = ['internet']
