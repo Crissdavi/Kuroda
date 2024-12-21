d2e64007c8744c16251a5410ed6d054d5942e319
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-12-21 12:37:45 -0500

    Update game-math.js

--- plugins/game-math.js
+++ plugins/game-math.js
@@ -2,22 +2,22 @@
 let handler = async (m, { conn, args, usedPrefix }) => {
   conn.math = conn.math ? conn.math : {}
   if (args.length < 1) throw `
-Mode: ${Object.keys(modes).join(' | ')}
-Contoh penggunaan: ${usedPrefix}math medium
+Modo: ${Object.keys(modes).join(' | ')}
+Ejemplo: ${usedPrefix}math noob
 `.trim()
   let mode = args[0].toLowerCase()
   if (!(mode in modes)) throw `
-Mode: ${Object.keys(modes).join(' | ')}
-Contoh penggunaan: ${usedPrefix}math medium
+Modo: ${Object.keys(modes).join(' | ')}
+Ejemplo: ${usedPrefix}math noob
 `.trim()
   let id = m.chat
-  if (id in conn.math) return conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.math[id][0])
+  if (id in conn.math) return conn.reply(m.chat, 'Todavia hay preguntas sin responder', conn.math[id][0])
   let math = genMath(mode)
   conn.math[id] = [
-    await conn.reply(m.chat, `Berapa hasil dari *${math.str}*?\n\nTimeout: ${(math.time / 1000).toFixed(2)} detik\nBonus Jawaban Benar: ${math.bonus} XP`, m),
+    await conn.reply(m.chat, `Cual es el resultado de  *${math.str}*?\n\nTiempo: ${(math.time / 1000).toFixed(2)} detik\nBonus por la respuesta correcta: ${math.bonus} XP`, m),
     math, 4,
     setTimeout(() => {
-      if (conn.math[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah ${math.result}`, conn.math[id][0])
+      if (conn.math[id]) conn.reply(m.chat, `Se acabo el tiempo!\nla respuesta es  ${math.result}`, conn.math[id][0])
       delete conn.math[id]
     }, math.time)
   ]
