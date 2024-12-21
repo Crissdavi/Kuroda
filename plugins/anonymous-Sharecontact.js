93579ef420475009c54607f6041e914aa812076f
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-12-21 09:27:18 -0500

    Update and rename anonymous-sendkontak.js to anonymous-Sharecontact.js

--- plugins/anonymous-Sharecontact.js
+++ plugins/anonymous-Sharecontact.js
@@ -4,7 +4,7 @@ async function handler(m, { command, conn, text }) {
 	this.anonymous = this.anonymous ? this.anonymous : {}
 	let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 	let room = Object.values(this.anonymous).find(room => room.check(who))
-	if (!room) throw 'kamu tidak berada di anonymous chat'
+	if (!room) throw 'No estas en un chat anonimo '
 	let other = room.other(who)
   var name
   if (text) name = text
@@ -12,14 +12,14 @@ async function handler(m, { command, conn, text }) {
 	var number = who.split('@')[0]
 	let tks = `➔ Nomor: ${m.sender.split`@`[0]}
 ➔ Nama: ${name}`
-    this.reply(m.chat, 'Sedang Menggirimkan Kontak...')
-	if (other) this.reply(other, `Partner mengirimkan kontak kepadamu`)
+    this.reply(m.chat, 'enviando contacto...')
+	if (other) this.reply(other, `Tu amig@ te ha compartido un comtacto `)
 	if (other) this.sendFile(other, await conn.profilePictureUrl(m.sender, 'image').catch(_ => './src/avatar_contact.png'), '', `${htki} ᴀɴᴏɴʏᴍᴏᴜs ᴄʜᴀᴛs ${htka}\n` + tks, 0,  { mentionedJid: [m.sender]})
 }
 
-handler.help = ['sendkontak']
+handler.help = ['sendcontact']
 handler.tags = 'anonymous'
-handler.command = /^(sendkontak)$/i
+handler.command = /^(sendcontact)$/i
 handler.private = true
 
 export default handler
